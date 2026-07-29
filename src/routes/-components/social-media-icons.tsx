1d3690e74e77a98dbbc67227d723fb154dcee7be
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-28 22:53:16 -0700

    just use img tags for the svgs

--- src/routes/-components/social-media-icons.tsx
+++ src/routes/-components/social-media-icons.tsx
@@ -1,57 +1,32 @@
-import { cn } from "../../../@/lib/utils";
-import Bluesky from "../../assets/icons/socials/bluesky.svg?react";
-import Twitch from "../../assets/icons/socials/twitch.svg?react";
-
-type IconProps = {
-  variant: "twitch" | "youtube" | "bsky";
-};
-function Icon({ variant }: IconProps) {
-  const shared = "w-auto h-12";
-  const hover = "hover:brightness-150";
-
-  switch (variant) {
-    case "bsky":
-      return (
-        <a href="https://bsky.app/profile/bashprime.com" target="_blank">
-          <Bluesky
-            className={cn("fill-[#1185fe]", shared, hover)}
-            title="Bluesky"
-            alt="Bluesky logo"
-          />
-        </a>
-      );
-    case "twitch":
-      return (
-        <a href="https://twitch.tv/bashprime" target="_blank">
-          <Twitch
-            className={cn("fill-[#9146ff]", shared, hover)}
-            fill="white"
-            title="Bluesky"
-            alt="Bluesky logo"
-          />
-        </a>
-      );
-    case "youtube":
-      return (
-        <a href="https://youtube.com/bashprime" target="_blank">
-          <img
-            src="/src/assets/icons/socials/youtube.png"
-            className={cn("w-auto h-16", hover)}
-          />
-        </a>
-      );
-  }
-}
+import { cn } from "@/lib/utils";
 
 export function SocialMediaIcons() {
+  const size = "w-auto h-12";
+  const hover = "hover:brightness-150";
+
   return (
     <div
       className="flex flex-row gap-6 items-center"
       data-name="social-media-icons"
     >
-      <Icon variant="twitch" />
-      <Icon variant="youtube" />
-      <Icon variant="bsky" />
+      <a href="https://twitch.tv/bashprime" target="_blank">
+        <img
+          src="./src/assets/icons/socials/twitch.svg"
+          className={cn(size, hover)}
+        />
+      </a>
+      <a href="https://youtube.com/bashprime" target="_blank">
+        <img
+          src="/src/assets/icons/socials/youtube.png"
+          className={cn("w-auto h-16", hover)}
+        />
+      </a>
+      <a href="https://bsky.app/profile/bashprime.com" target="_blank">
+        <img
+          src="./src/assets/icons/socials/bluesky.svg"
+          className={cn(size, hover)}
+        />
+      </a>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +13,6 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
-    svgr(),
   ],
   resolve: {
     alias: {
