53a72404dd6149de979c40785ed3b468c5d22627
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-28 19:10:12 -0700

    config, add logo

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from './routeTree.gen';
 
 // Create a new router instance
 const router = createRouter({ routeTree })
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,25 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
-    <div className="p-2 text-red-400">
-      <h3>Welcome Home!</h3>
+    <div data-name="index-route-parent">
+      <div
+        className="flex flex-row items-center gap-4 justify-center"
+        data-name="header"
+      >
+        <img
+          src="./src/assets/bp-logo-transparent.svg"
+          className="w-auto h-24"
+        />
+        <h1 className="text-7xl font-bold">
+          <span className="text-bashprime-red">Bash</span>
+          <span className="text-bashprime-yellow">Prime</span>
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
