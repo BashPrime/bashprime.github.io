90e118d02614f0a27353e970a670ee7bae16a1be
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-28 23:41:56 -0700

    root styling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,18 +3,7 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const RootLayout = () => (
   <>
-    {/* <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>
-      <Link to="/hint-tracker" className="[&.active]:font-bold">
-        Hint Tracker
-      </Link>
-    </div>
-    <hr /> */}
-    <div className="p-6">
-      <Outlet />
-    </div>
+    <Outlet />
     <TanStackRouterDevtools />
   </>
 );
