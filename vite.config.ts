62f3d8fac41b713d5dfe7e916c1bba98270f6f5e
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-28 22:44:49 -0700

    resolve @ path

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import tailwindcss from '@tailwindcss/vite';
-import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter({
-      target: 'react',
+      target: "react",
       autoCodeSplitting: true,
     }),
     react(),
     tailwindcss(),
     svgr(),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
