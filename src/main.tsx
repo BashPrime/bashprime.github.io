6eade34c7419b4c4f314da9388a38490dd762dfa
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-28 23:33:42 -0700

    will memoryHistory work???

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router';
+import { RouterProvider, createMemoryHistory, createRouter } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
 
-const hashHistory = createHashHistory();
-
+const memoryHistory = createMemoryHistory({
+  initialEntries: ['/'],
+})
 // Create a new router instance
-const router = createRouter({ routeTree, history: hashHistory })
+const router = createRouter({ routeTree, history: memoryHistory })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
